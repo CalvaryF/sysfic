7dddcdd537417a657208dff8169a1cc7d21b7536
Laptop 11 <laptop11@FNDRco-MBPro-10.lan>
2021-01-06 14:06:02 -0800

    multiImage mobile

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,31 +5,31 @@ class Header extends Component {
   render() {
     return (
       <div className="footerMain">
-        <div className="contact">
-          {" "}
-          For business inquiries: <br></br>
+        <div className="links">
+          <div className="linkContainer">
+            <Link to="/" className="footerLink">
+              Youtube{" "}
+            </Link>
+            <Link to="/" className="footerLink">
+              Instagram{" "}
+            </Link>
+            <Link to="/" className="footerLink">
+              Linkedin{" "}
+            </Link>
+            <Link to="/" className="footerLink">
+              Medium{" "}
+            </Link>
+            <Link to="/" className="footerLink">
+              Twitter{" "}
+            </Link>
+          </div>
+          <div className="copyright"> &copy; Calvary Fisher 2021 </div>
+        </div>
+        <div className="contactContainer">
+          <div className="contact"> For business inquiries:</div>
           <Link to="/" className="contactButton">
             Email me &#8594;
           </Link>{" "}
-        </div>
-
-        <div className="links">
-          <Link to="/" className="footerLink">
-            Youtube{" "}
-          </Link>
-          <Link to="/" className="footerLink">
-            Instagram{" "}
-          </Link>
-          <Link to="/" className="footerLink">
-            Linkedin{" "}
-          </Link>
-          <Link to="/" className="footerLink">
-            Medium{" "}
-          </Link>
-          <Link to="/" className="footerLink">
-            Twitter{" "}
-          </Link>
-          <div className="copyright"> &copy; Calvary Fisher 2021 </div>
         </div>
       </div>
     );
--- src/info.jsx
+++ src/info.jsx
@@ -1,43 +1,53 @@
-import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
 
-
-function Info ()
-{
-    return (
-        <div  className ="flex">
-            
-            <div className ="por">
-
-          
-                <div className ="imageCard">
-                     <div className="infoHead"> 
-                    <span className="cardTitle">@cal</span> 
-                 <div className="dot mantle"></div>   
-                 </div>
-                 <img  className = "infoImage"width ="400px;"src = "/images/info.png" alt ="portrait"/>
-                 <div className="infoTextDiv">
-                     <span className="infoText"> Calvary Fisher is a speculative designer exploring remote possibilities in system-centered and product design. He lives inLos Angeles, California, and enjoys reading, running, and reality.
-                     </span>
-                 </div>
-                </div>
-
-                <div className ="cardOutline">
-                     
-                     </div>
-     
-                     <div className ="cardOutline">
-                     
-                     </div>
-
-               
-
-            </div>
-           
-            
-
+function Info() {
+  return (
+    <div className="flex">
+      <div className="por">
+        <div className="imageCard">
+          <div className="infoHead">
+            <span className="cardTitle">@calv</span>
+            <div className="dot crust"></div>
+          </div>
+          <img
+            className="infoImage"
+            width="400px;"
+            src="/images/info.png"
+            alt="portrait"
+          />
+          <div className="infoTextDiv">
+            <span className="infoText">
+              {" "}
+              Calvary Fisher is a speculative designer exploring remote
+              possibilities in system-centered design. He lives inLos Angeles,
+              California, and enjoys reading, running, and reality.
+            </span>
+          </div>
         </div>
-    );
+
+        <div className="imageCard">
+          <div className="linkBox">
+            <Link to="/" className="exLink">
+              Instagram{" "}
+            </Link>
+            <Link to="/" className="exLink">
+              Twitter
+            </Link>
+            <Link to="/" className="exLink">
+              Tiktok
+            </Link>
+            <Link to="/" className="exLink">
+              Medium
+            </Link>
+            <Link to="/" className="exLink">
+              Linkedin
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Info;
